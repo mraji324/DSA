67c40e1710b384a4e042e238aa2a0de365e952c6
Hindu <rajuhindur@gmail.com>
2025-11-21 11:10:16 +0530

    Add files via upload

---
+++ ReverseInteger.java
@@ -0,0 +1,14 @@
+public class ReverseInteger {
+    public static void main(String[] args) {
+        int num = 12345;   // example input
+        int reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;         
+            reversed = reversed * 10 + digit; 
+            num /= 10;                     
+        }
+
+        System.out.println("Reversed number: " + reversed);
+    }
+}
